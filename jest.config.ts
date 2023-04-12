5ec57527d82978236df1b7627d2108bc0473eb1c
rijan adhikari <rijanadhikari@gmail.com>
2023-04-12 10:12:39 +0900

    chore: updated jest config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  roots: ['<rootDir>/tests/'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+}
